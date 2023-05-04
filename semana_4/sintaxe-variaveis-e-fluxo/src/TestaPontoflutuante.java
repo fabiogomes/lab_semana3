4c81f76334175f3f1454d56e2cc70b1b41964781
david-e-c <davidedson@outlook.com>
2023-05-04 13:23:18 -0300

    Adicionados arquivos de estudo e trello da Semana 4

---
+++ semana_4/sintaxe-variaveis-e-fluxo/src/TestaPontoflutuante.java
@@ -0,0 +1,24 @@
+
+public class TestaPontoflutuante {
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+        System.out.println("meu salário é " + salario);
+        
+        double divisao = 3.14 / 2;
+        System.out.println(divisao);
+        
+        int outraDivisao = 5 / 2;
+        System.out.println(outraDivisao);
+        
+        double novaTentativa = 5 / 2;
+        System.out.println(novaTentativa);
+        		
+        
+        double novamente = 5.0 / 2;
+        System.out.println(novamente);
+        		
+        		
+	}
+
+}
